af27b0aa46f01c97714d8af262914e25a3402b02
Itamar Shabtai <itamar.shabtai.1@gmail.com>
2022-03-24 17:31:15 +0200

    Added Controls screen and fixed a bug

--- game.asm
+++ game.asm
@@ -29,6 +29,8 @@ by_offset dw 00h
 
 speed dw 05h
 
+plane_x_0 dw 00h
+
 ; building width and height
 building_width dw 00h
 building_height dw 00h
@@ -44,6 +46,13 @@ db 0h,2 dup(64h),0h,4 dup(04h),3 dup(0h),13 dup(07h),0h,8 dup(04h),0h,12 dup(07h
 draw_delete_plane_x dw 00h
 draw_delete_plane_y dw 00h
 
+filename db 'start.bmp',0
+filehandle dw ?
+Header db 54 dup (0)
+Palette db 256*4 dup (0)
+ScrLine db 320 dup (0)
+ErrorMsg db 'Error', 13, 10 ,'$'
+
 CODESEG
 
 proc move_to_graphics_mode
@@ -51,6 +60,106 @@ proc move_to_graphics_mode
     mov al, 13h
     int 10h
 endp
+
+proc OpenFile
+    ; Open file
+    mov ah, 3Dh
+    xor al, al
+    mov dx, offset filename
+    int 21h
+    jc openerror
+    mov [filehandle], ax
+    ret
+    openerror:
+        mov dx, offset ErrorMsg
+        mov ah, 9h
+        int 21h
+        ret
+endp OpenFile
+
+proc ReadHeader
+    ; Read BMP file header, 54 bytes
+    mov ah,3fh
+    mov bx, [filehandle]
+    mov cx,54
+    mov dx,offset Header
+    int 21h
+    ret
+endp ReadHeader
+
+proc ReadPalette
+    ; Read BMP file color palette, 256 colors * 4 bytes (400h)
+    mov ah,3fh
+    mov cx,400h
+    mov dx,offset Palette
+    int 21h
+    ret
+endp ReadPalette
+
+proc CopyPal
+    ; Copy the colors palette to the video memory
+    ; The number of the first color should be sent to port 3C8h
+    ; The palette is sent to port 3C9h
+    mov si,offset Palette
+    mov cx,256
+    mov dx,3C8h
+    mov al,0
+    ; Copy starting color to port 3C8h
+    out dx,al
+    ; Copy palette itself to port 3C9h
+    inc dx
+    PalLoop:
+        ; Note: Colors in a BMP file are saved as BGR values rather than RGB .
+        mov al,[si+2] ; Get red value .
+        shr al,2 ; Max. is 255, but video palette maximal
+        ; value is 63. Therefore dividing by 4.
+        out dx,al ; Send it .
+        mov al,[si+1] ; Get green value .
+        shr al,2
+        out dx,al ; Send it .
+        mov al,[si] ; Get blue value .
+        shr al,2
+        out dx,al ; Send it .
+        add si,4 ; Point to next color .
+        ; (There is a null chr. after every color.)
+    loop PalLoop
+    ret
+endp CopyPal
+
+proc CopyBitmap
+    ; BMP graphics are saved upside-down .
+    ; Read the graphic line by line (200 lines in VGA format),
+    ; displaying the lines from bottom to top.
+    mov ax, 0A000h
+    mov es, ax
+    mov cx,200
+    PrintBMPLoop :
+        push cx
+        ; di = cx*320, point to the correct screen line
+        mov di,cx
+        shl cx,6
+        shl di,8
+        add di,cx
+        ; Read one line
+        mov ah,3fh
+        mov cx,320
+        mov dx,offset ScrLine
+        int 21h
+        ; Copy one line into video memory
+        cld ; Clear direction flag, for movsb
+        mov cx,320
+        mov si,offset ScrLine
+        rep movsb ; Copy line to the screen
+            ;rep movsb is same as the following code :
+            ;mov es:di, ds:si
+            ;inc si
+            ;inc di
+            ;dec cx
+            ;... ;loop until cx=0
+        pop cx
+    loop PrintBMPLoop
+    ret
+endp CopyBitmap
 
 proc change_bg_color
     mov ah, 06h
@@ -565,12 +674,42 @@ proc update_super_ammo
     ret
 endp
 
+proc color_top
+    mov cx, 00h
+    mov dx, 00h
+    mov ah, 0Ch
+    mov al, 00h
+    color_top_loop:
+        int 10h
+	inc cx
+	cmp cx, 320
+	jg color_top_exit
+	jmp color_top_loop
+    color_top_exit:
+    ret
+endp
+
 Start:
     mov ax, @data
     mov ds, ax
 
     call move_to_graphics_mode
-    jmp restart_skip
+
+    show_controls:
+        ; Process BMP file
+        call OpenFile
+        call ReadHeader
+        call ReadPalette
+        call CopyPal
+        call CopyBitmap
+	    call color_top
+        ; Wait for key press
+        mov ah,1
+        int 21h
+    
+        call move_to_graphics_mode
+	    jmp restart
+
     restart:
         mov si, offset bx_offset_array
         call clear_array
@@ -589,7 +728,7 @@ Start:
         mov [speed], 05h
         mov [x_offset], 00h
         mov [y_offset], 00h
-    restart_skip:
+
     call change_bg_color
     call draw_plane
     call draw_all_buildings
@@ -598,6 +737,7 @@ Start:
 update_plane_y:
     add [y_offset], 10h
     mov [x_offset], 00h
+    mov [plane_x_0], 0001h
     mov [bullets_left], 05h
     mov [super_bullets_left], 01h
     call update_super_ammo
@@ -642,15 +782,28 @@ update_frame:
     call delete_plane
     mov si, offset x_offset
     mov ax, [si]
-    cmp ax, 320
+    cmp ax, 310
     jge update_plane_y_middle
     cmp [y_offset], 0B0h
     jge next_level
+    cmp [plane_x_0], 0001h
+    je skip_add_plane_x
     add ax, 0ah
     mov [si], ax
+    jmp skip_skip_add_plane_x
+    skip_add_plane_x:
+        mov [plane_x_0], 0000h
+    skip_skip_add_plane_x:
 	call draw_plane
     call update_bullets
     jmp mainloop
+
+jmp middles_skip
+show_controls_middle:
+    jmp show_controls
+update_frame_middle:
+    jmp update_frame
+middles_skip:
 spacebar_pressed:
     cmp [bullets_left], 00h
     jg spacebar_pressed_create_bullet
@@ -694,6 +847,8 @@ mainLoop:
     je restart_middle
     cmp al, 'p'
     je pause_game
+    cmp al, 'z'
+    je show_controls_middle
     jmp update_frame
 
 jmp pause_game_skip
@@ -701,7 +856,7 @@ pause_game:
     mov ah, 00h
     int 16h
     cmp al, 'p'
-    je update_frame
+    je update_frame_middle
     jmp pause_game
 pause_game_skip:
 
